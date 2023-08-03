116da7b20fd0cd5cd749fc55f7db7865ca2ba73d
Juan Manuel Perez <jperezflorez@newrelic.com>
2023-08-03 15:31:20 +0200

    fix goporto imports

--- receiver/statsdreceiver/internal/testutil/metric.go
+++ receiver/statsdreceiver/internal/testutil/metric.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package testutil // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/statsdreceiver/internal/transport/testutil"
+package testutil // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/statsdreceiver/internal/testutil"
 
 import (
 	"fmt"
